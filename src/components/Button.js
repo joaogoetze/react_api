1741e85b1438c14eb78d102adc2015993638193a
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-10-28 20:58:52 -0300

    adding routes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,18 @@
+import Dashboard from "../pages/Dashboard";
+import React, {useState} from 'react';
+import { useNavigate } from "react-router-dom";
+
 function MyButton() {
-    return (
-      <button>I'm a button</button>
+  const navigate = useNavigate();
+    
+  const handleClick = () => {
+    navigate('/dashboard');
+  }
+  
+  return (
+      <div> 
+          <button onClick={handleClick}>I'm a button</button>
+      </div>
     );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <button>Listar carros</button>
+            <button>Listar carros por ID</button>
+            <button>Criar carro</button>
+            <button>Editar carro</button>
+            <button>Deletar carro</button>
+        </div>
+    );
+}
+
+export default Dashboard;
