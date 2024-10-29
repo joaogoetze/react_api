07f2bb9cf210ee8fb4a112495d35316be4f21b6c
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-10-29 20:15:05 -0300

    adding checlist and drectionable button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from '../logo.svg';
+import MyButton from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-    return(
-        <div className="App">
-        <header className="App-header">
+  const navigate = useNavigate();
+
+  const goToDashboard = () => {
+    navigate('/dashboard');
+  }
+
+  return(
+    <div className="App">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -12,9 +20,13 @@ function Home() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>  
+        <br></br>
+        <MyButton />
+        <br></br>
+        <button onClick={goToDashboard}>Dashboard</button> 
+      </header> 
     </div>
-    );
+  );
 }
 
 export default Home;
