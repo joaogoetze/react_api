9e023cc34e14ece1ca0884dd88f2b8e824b39089
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-11-01 10:40:26 -0300

    adding reusable Button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import ResusableButton from '../components/ReusableButton';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+    
+    const navigate = useNavigate();
+
+    //função que consegue levar apenas para uma rota do próprio sistema
+    const goToLink = (route) => {
+        navigate(route);
+    }
+
+    //função que confere se é um link externo ou é uma rota interna do sistema, e transfere para o certo
+    const goToDestination = (destination) => {
+        if(destination.startsWith("http"))
+        {
+            window.open(destination, "_blank");
+        }
+        else
+        {
+            navigate(destination);
+        }
+    }
+    
     return (
         <div>
-            <button>Listar carros</button>
-            <button>Listar carros por ID</button>
-            <button>Criar carro</button>
-            <button>Editar carro</button>
-            <button>Deletar carro</button>
+            <ResusableButton onPress={() => goToLink('/learnreact')} title='Leva para Rota'/>
+            <ResusableButton onPress={() => goToDestination('https://youtube.com')} title='Leva para Youtube'/>
         </div>
     );
 }
